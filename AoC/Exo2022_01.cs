a4d13ec1064fbe12f4bd646bb2006dd865053a74
Quentin <qlecavorzin@tilakhealthcare.com>
2023-11-20 16:20:01 +0100

    exo1 - v1

---
+++ AoC/Exo2022_01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("C:\\Prototypes_Perso\\AdventOfCode\\AoC\\Sample.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+            List<Elf> elves = [new Elf(0)];
+            int counter = 0;
+
+            for (int i = 0; i < textSplitted.Length; i++)
+            {
+                if (textSplitted[i] == "")
+                {
+                    elves.Add(new Elf(0));
+                    counter++;
+                    Console.WriteLine($"elf {counter - 1} : {elves[counter - 1].calories}");
+                }
+                else
+                {
+                    elves[counter].calories += int.Parse(textSplitted[i]);
+                }
+            }
+
+            Console.WriteLine($"The fatest elf has {GetFatestElf(elves).calories} calogies.");
+        }
+
+        public static Elf GetFatestElf(List<Elf> list)
+        {
+            Elf tmp = new Elf(0);
+            foreach (Elf elf in list)
+            {
+                tmp = tmp.calories < elf.calories ? elf : tmp;
+            }
+            return tmp;
+        }
+    }
+
+    public class Elf
+    {
+        public int calories;
+
+        public Elf(int pCalories) 
+        {
+            calories = pCalories;
+        }
+    }
+}
---
+++ AoC/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC
+{
+    public static class Utils
+    {
+        public static string ReadFile(string path)
+        {
+            string fileContent = File.ReadAllText(path)
+                        .TrimEnd('\n'); //Remove the last \n that is usually outside the scope of the puzzle
+
+            return fileContent;
+            //Console.WriteLine(fileContent);
+        }
+    }
+
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void PuzzleTest()
+        {
+            Assert.AreEqual(3, int.Parse("3"));
+        }
+    }
+}
