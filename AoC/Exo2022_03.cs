3f2f9b8188e4d2d7f1860c51aedcbb00095e7e93
Quentin <qlecavorzin@tilakhealthcare.com>
2023-11-24 11:44:12 +0100

    Exo 3 files creation

---
+++ AoC/Exo2022_03.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC2022_Exo3
+{
+    /*
+    Infos
+    */
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("C:\\Prototypes_Perso\\AdventOfCode\\AoC\\Exo2022_03.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+        }
+    }
+}
