bd74c9c8f24019bc78b66ad2a3d579d057671931
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-01 09:53:08 +0100

    2023 01 creation

---
+++ AoC/Exo2023_01.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using NuGet.Frameworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC2023_Exo1
+{
+    /*
+    Infos
+    */
+    public class Program
+    {
+        // ICI LA LES VARIABLES, PAS EN DESSOUS -_-
+        public static string[] instrSeparators = new string[]
+        {
+            "move",
+            "from",
+            "to"
+        };
+
+        public static void Main()
+        {
+            string text = Utils.ReadFile("C:\\Prototypes_Perso\\AdventOfCode\\AoC\\Exo2023_01.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+        }
+    }
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test1Digit()
+        {
+
+        }
+    }
+}
