d6369beea19543391d717ccbde0fa932a032df7d
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-06 16:40:43 +0100

    2023 exo 4 creation

---
+++ AoC/Exo2023_04.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using NuGet.Frameworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC2023_Exo4
+{
+    /*
+     * On cherche 13 dans l'exemple
+    */
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AdventOfCode\\AoC\\Exo2023_04.txt");
+            string[] textSplitted = text.Split(Environment.NewLine);
+        }
+    }
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
