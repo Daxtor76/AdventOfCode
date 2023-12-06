e9c2fa0cc41f5b5d04036fe4f9578d582b5d7c48
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-06 16:23:05 +0100

    2023 exo 3 part 2

--- AoC/Exo2023_03.cs
+++ AoC/Exo2023_03.cs
@@ -20,8 +20,7 @@ using static System.Net.Mime.MediaTypeNames;
 namespace AoC2023_Exo3
 {
     /*
-     * On cherche 4361 dans l'exemple
-     * 492202 too low
+     * On cherche 467835 dans l'exemple
     */
     public class Program
     {
@@ -34,28 +33,122 @@ namespace AoC2023_Exo3
             List<Vector2> closedList = new List<Vector2>();
             int gridMaxX = textSplitted[1].Length;
             int gridMaxY = textSplitted.Length;
-            int total = 0;
 
-            // Parse chaque ligne
-            // Si on tombe sur un digit => check all neighbours (8 directions)
-            // Si symbole => check gauche et droite jusqu'à trouver un char != digit
-            // Concat et créer un int à garder pour additionner après
-            
-            for (int i =  0; i < gridMaxX; i++)
+            // STEP 1
+            //Console.WriteLine($"Total Step 1: {AddNumbersNearAuthorizedSymboles(engineScheme, closedList, gridMaxX, gridMaxY)}");
+
+            // STEP 2
+            Console.WriteLine($"Total Step 2: {MultiplyNumbersNearGears(engineScheme, closedList, gridMaxY)}");
+        }
+
+        private static int MultiplyNumbersNearGears(Dictionary<Vector2, char> engineScheme, List<Vector2> closedList, int gridMaxY)
+        {
+            int total = 0;
+            foreach (Vector2 gear in GetGearsPos(engineScheme, closedList, 2))
+            {
+                int gearRatio = 1;
+                List<int> gearNeighboursNumbers = GetNeighboursNumbers(engineScheme, gridMaxY, closedList, gear);
+                if (gearNeighboursNumbers.Count() == 2)
+                {
+                    foreach (int value in gearNeighboursNumbers)
+                    {
+                        gearRatio *= value;
+                        Console.WriteLine($"{gearRatio}");
+                    }
+                    total += gearRatio;
+                }
+            }
+
+            return total;
+        }
+        public static List<Vector2> GetGearsPos(Dictionary<Vector2, char> engineScheme, List<Vector2> closedList, int amountOfDigitNeighbours)
+        {
+            List<Vector2> result = new List<Vector2>();
+            foreach (KeyValuePair<Vector2, char> kvp in engineScheme)
+            {
+                if (kvp.Value == 42)
+                {
+                    result.Add(kvp.Key);
+                }
+            }
+
+            return result;
+        }
+        public static List<int> GetNeighboursNumbers(Dictionary<Vector2, char> db, int gridMaxY, List<Vector2> closedDB, Vector2 pos)
+        {
+            Vector2 right = pos + new Vector2(0, 1);
+            Vector2 downRight = pos + new Vector2(-1, 1);
+            Vector2 down = pos + new Vector2(-1, 0);
+            Vector2 downLeft = pos + new Vector2(-1, -1);
+            Vector2 left = pos + new Vector2(0, -1);
+            Vector2 upLeft = pos + new Vector2(1, -1);
+            Vector2 up = pos + new Vector2(1, 0);
+            Vector2 upRight = pos + new Vector2(1, 1);
+
+            List<int> result = new List<int>();
+
+            if (db.ContainsKey(right) && char.IsDigit(db[right]) && !closedDB.Contains(right))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, right);
+                result.Add(test);
+            }
+            if (db.ContainsKey(downRight) && char.IsDigit(db[downRight]) && !closedDB.Contains(downRight))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, downRight);
+                result.Add(test);
+            }
+            if (db.ContainsKey(down) && char.IsDigit(db[down]) && !closedDB.Contains(down))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, down);
+                result.Add(test);
+            }
+            if (db.ContainsKey(downLeft) && char.IsDigit(db[downLeft]) && !closedDB.Contains(downLeft))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, downLeft);
+                result.Add(test);
+            }
+            if (db.ContainsKey(left) && char.IsDigit(db[left]) && !closedDB.Contains(left))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, left);
+                result.Add(test);
+            }
+            if (db.ContainsKey(upLeft) && char.IsDigit(db[upLeft]) && !closedDB.Contains(upLeft))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, upLeft);
+                result.Add(test);
+            }
+            if (db.ContainsKey(up) && char.IsDigit(db[up]) && !closedDB.Contains(up))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, up);
+                result.Add(test);
+            }
+            if (db.ContainsKey(upRight) && char.IsDigit(db[upRight]) && !closedDB.Contains(upRight))
+            {
+                int test = GetNumberFromPos(db, gridMaxY, closedDB, upRight);
+                result.Add(test);
+            }
+
+            return result;
+        }
+
+        private static int AddNumbersNearAuthorizedSymboles(Dictionary<Vector2, char> engineScheme, List<Vector2> closedList, int gridMaxX, int gridMaxY)
+        {
+            int total = 0;
+            for (int i = 0; i < gridMaxX; i++)
             {
                 for (int y = 0; y < gridMaxY; y++)
                 {
                     Vector2 pos = new Vector2(i, y);
                     if (engineScheme.ContainsKey(pos) && !closedList.Contains(pos) && char.IsDigit(engineScheme[pos]))
                     {
-                        if (CheckNeighbours(engineScheme, closedList, pos))
+                        if (IsNeighbourAnAuthorizedSymbole(engineScheme, closedList, pos))
                         {
                             total += GetNumberFromPos(engineScheme, gridMaxY, closedList, pos);
                         }
                     }
                 }
             }
-            Console.WriteLine($"{total}");
+            return total;
         }
 
         private static int GetNumberFromPos(Dictionary<Vector2, char> db, int maxY, List<Vector2> closedDB, Vector2 pos)
@@ -80,12 +173,8 @@ namespace AoC2023_Exo3
                         break;
                     }
                 }
-            }
-            Console.WriteLine($"With left numbers: {tmp}");
-            char[] tmpArray = tmp.ToCharArray();
-            Array.Reverse(tmpArray);
-            string reversedTmp = new string(tmpArray);
-            Console.WriteLine($"With right numbers: {reversedTmp}");
+            };
+            string reversedTmp = ReverseLeftNumbers(tmp);
             for (int y = 0; y < maxY - pos.Y; y++)
             {
                 Vector2 newPos = new Vector2(pos.X, pos.Y + y);
@@ -103,12 +192,20 @@ namespace AoC2023_Exo3
                 }
             }
 
-            Console.WriteLine($"number to add: {reversedTmp}");
+            //Console.WriteLine($"number to add: {reversedTmp}");
 
             return int.Parse(reversedTmp);
         }
 
-        public static bool CheckNeighbours(Dictionary<Vector2, char> db, List<Vector2> closedDB, Vector2 pos)
+        private static string ReverseLeftNumbers(string tmp)
+        {
+            char[] tmpArray = tmp.ToCharArray();
+            Array.Reverse(tmpArray);
+            string reversedTmp = new string(tmpArray);
+            return reversedTmp;
+        }
+
+        public static bool IsNeighbourAnAuthorizedSymbole(Dictionary<Vector2, char> db, List<Vector2> closedDB, Vector2 pos)
         {
             Vector2 right = pos + new Vector2(0, 1);
             Vector2 downRight = pos + new Vector2(-1, 1);
