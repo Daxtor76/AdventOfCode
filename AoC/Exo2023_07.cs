b03ca9808c2e79f298630019da037f51f04760bb
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-16 16:56:53 +0100

    2023 exo 7 - beginning

--- AoC/Exo2023_07.cs
+++ AoC/Exo2023_07.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
 using System.Numerics;
+using System.Runtime.ExceptionServices;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -26,15 +27,112 @@ namespace AoC2023_Exo7
     */
     public class Program
     {
+        public static List<char> cardsValues = new List<char>
+        {
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            'T',
+            'J',
+            'Q',
+            'K',
+            'A'
+        };
+        public static List<string> handTypesValues = new List<string>
+        {
+            "HC",
+            "OP",
+            "TP",
+            "TOAK",
+            "FH",
+            "FoOAK",
+            "FiOAK"
+        };
+
+        public static Dictionary<string, int> hands = new Dictionary<string, int>();
         public static void Main()
         {
-            string text = Utils.ReadFile("C:\\Prototypes_Perso\\AdventOfCode\\AoC\\Exo2023_07.txt");
+            string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AdventOfCode\\AoC\\Exo2023_07.txt");
             string[] textSplitted = text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
             Stopwatch jackeline = Stopwatch.StartNew();
+
+            hands = GetHands(textSplitted);
+            foreach (string hand in hands.Keys)
+            {
+                Console.WriteLine(GetHandType(hand));
+            }
+
             Console.WriteLine(jackeline.ElapsedMilliseconds);
         }
+
+        public static char CompareCards(char card1, char card2)
+        {
+            if (card1 == card2)
+                return 'E';
+            else
+                return GetCardValue(card1) > GetCardValue(card2) ? card1 : card2;
+        }
+
+        public static int GetCardValue(char card)
+        {
+            return cardsValues.IndexOf(card);
+        }
+
+        public static string GetHandType(string hand)
+        {
+            Dictionary<char, int> cardCount = new Dictionary<char, int>();
+
+            foreach (char card in hand)
+            {
+                if (!cardCount.ContainsKey(card))
+                    cardCount.Add(card, 1);
+                else
+                    cardCount[card]++;
+            }
+
+            if (cardCount.Count() == 1)
+                return "FiOAK";
+            else if (cardCount.Count() == 2)
+                return "FoOAK";
+            else if (cardCount.Count() == 3)
+            {
+                if (cardCount.ContainsValue(3))
+                    return "TOAK";
+                else
+                    return "TP";
+            }
+            else if (cardCount.Count() == 4)
+                return "OP";
+            else
+                return "HC";
+        }
+
+        public static int GetHandTypeValue(string handType)
+        {
+            return handTypesValues.FindIndex(t => t == handType);
+        }
+
+        public static Dictionary<string, int> GetHands(string[] input)
+        {
+            Dictionary<string, int> hands = new Dictionary<string, int>();
+
+            foreach (string line in input)
+            {
+                string[] splittedInput = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                hands.Add(splittedInput[0], int.Parse(splittedInput[1]));
+                //Console.WriteLine($"hand: {splittedInput[0]} - bid: {splittedInput[1]}");
+            }
+
+            return hands;
+        }
     }
+
     [TestClass]
     public class Tests
     {
