0e6248b359dff8233b411dc068004b46378d89cb
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-16 15:47:50 +0100

    2023 exo 7 creation

---
+++ AoC/Exo2023_07.cs
@@ -0,0 +1,46 @@
+﻿using AoC2022_Exo1;
+using AoC2023_Exo5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using NuGet.Frameworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AoC2023_Exo7
+{
+    /*
+    */
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Utils.ReadFile("C:\\Prototypes_Perso\\AdventOfCode\\AoC\\Exo2023_07.txt");
+            string[] textSplitted = text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Stopwatch jackeline = Stopwatch.StartNew();
+            Console.WriteLine(jackeline.ElapsedMilliseconds);
+        }
+    }
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
