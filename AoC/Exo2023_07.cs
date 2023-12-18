7008b794a9f76d816886ddddfaac39aea377d81d
QuentinTilak <lecavorzin.quentin@gmail.com>
2023-12-18 14:05:38 +0100

    2023 exo 7 - try with Icomparable

--- AoC/Exo2023_07.cs
+++ AoC/Exo2023_07.cs
@@ -1,4 +1,5 @@
 ﻿using AoC2022_Exo1;
+using AoC2023_Exo4;
 using AoC2023_Exo5;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
@@ -24,6 +25,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace AoC2023_Exo7
 {
     /*
+     * 255260935 too high
     */
     public class Program
     {
@@ -54,7 +56,7 @@ namespace AoC2023_Exo7
             "FiOAK"
         };
 
-        public static Dictionary<string, int> hands = new Dictionary<string, int>();
+        public static List<Hand> hands = new List<Hand>();
         public static void Main()
         {
             string text = Utils.ReadFile("E:\\Projets\\AdventOfCode\\AdventOfCode\\AoC\\Exo2023_07.txt");
@@ -62,33 +64,106 @@ namespace AoC2023_Exo7
 
             Stopwatch jackeline = Stopwatch.StartNew();
 
-            hands = GetHands(textSplitted);
-            foreach (string hand in hands.Keys)
+            int test = 0;
+            hands = OrderHands(GetHands(textSplitted));
+            foreach (Hand hand in hands)
             {
-                Console.WriteLine(GetHandType(hand));
+                test += hand.GetHandScore();
+                //Console.WriteLine($"hand: {hand.cards} - bid: {hand.bid} - type: {hand.type} - value: {GetHandTypeValue(hand)} - rank: {hand.rank} - score: {hand.GetHandScore()}");
+                Console.WriteLine($"cards value: {GetCardsValue(hand)}");
             }
+            Console.WriteLine($"{test}");
 
             Console.WriteLine(jackeline.ElapsedMilliseconds);
         }
 
-        public static char CompareCards(char card1, char card2)
+        public static List<Hand> OrderHands(List<Hand> hands)
         {
-            if (card1 == card2)
-                return 'E';
+            //List<Hand> tmp = hands.OrderBy(v => GetHandTypeValue(v)).ThenBy(b => GetCardsValue(b)).ToList();
+            List<Hand> tmp = hands.OrderBy(GetHandTypeValue).ToList().Sort();
+
+            foreach (Hand hand in tmp)
+            {
+                hand.rank = tmp.IndexOf(hand) + 1;
+            }
+
+            return tmp;
+        }
+
+        public static int GetCardsValue(Hand hand)
+        {
+            string tmp = "";
+
+            foreach (char c in hand.cards)
+            {
+                tmp += cardsValues.FindIndex(t => t == c);
+            }
+
+            return int.Parse(tmp);
+        }
+
+        public static int GetHandTypeValue(Hand hand)
+        {
+            return handTypesValues.FindIndex(t => t == hand.type);
+        }
+
+        public static List<Hand> GetHands(string[] input)
+        {
+            List<Hand> hands = new List<Hand>();
+
+            foreach (string line in input)
+            {
+                string[] splittedInput = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Hand hand = new Hand(splittedInput[0], int.Parse(splittedInput[1]));
+                hands.Add(hand);
+            }
+
+            return hands;
+        }
+    }
+
+    public class Hand : IComparable
+    {
+        public string cards = "";
+        public string type = "";
+        public int bid = 0;
+        public int rank = 0;
+
+        public Hand(string _cards, int _bid)
+        {
+            cards = _cards;
+            bid = _bid;
+            type = GetHandType();
+        }
+
+        public int CompareTo(object obj)
+        {
+            string otherCards = obj as string;
+            if (otherCards != null)
+            {
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    if (cards[i] == otherCards[i])
+                        continue;
+
+                    return Program.cardsValues.FindIndex(c => c == cards[i]).CompareTo(Program.cardsValues.FindIndex(t => t == otherCards[i]));
+                }
+            }
             else
-                return GetCardValue(card1) > GetCardValue(card2) ? card1 : card2;
+                throw new ArgumentException("Other cards is empty");
         }
 
-        public static int GetCardValue(char card)
-        {
-            return cardsValues.IndexOf(card);
+        public int GetHandScore()
+        { 
+            return bid * rank;
         }
 
-        public static string GetHandType(string hand)
+        public string GetHandType()
         {
             Dictionary<char, int> cardCount = new Dictionary<char, int>();
 
-            foreach (char card in hand)
+            foreach (char card in cards)
             {
                 if (!cardCount.ContainsKey(card))
                     cardCount.Add(card, 1);
@@ -99,37 +174,20 @@ namespace AoC2023_Exo7
             if (cardCount.Count() == 1)
                 return "FiOAK";
             else if (cardCount.Count() == 2)
-                return "FoOAK";
-            else if (cardCount.Count() == 3)
             {
-                if (cardCount.ContainsValue(3))
-                    return "TOAK";
+                if (cardCount.ContainsValue(4) && cardCount.ContainsValue(1))
+                    return "FoOAK";
+                else if (cardCount.ContainsValue(3) && cardCount.ContainsValue(2))
+                    return "FH";
                 else
-                    return "TP";
+                    return "TOAK";
             }
+            else if (cardCount.Count() == 3)
+                return "TP";
             else if (cardCount.Count() == 4)
                 return "OP";
             else
                 return "HC";
-        }
-
-        public static int GetHandTypeValue(string handType)
-        {
-            return handTypesValues.FindIndex(t => t == handType);
-        }
-
-        public static Dictionary<string, int> GetHands(string[] input)
-        {
-            Dictionary<string, int> hands = new Dictionary<string, int>();
-
-            foreach (string line in input)
-            {
-                string[] splittedInput = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                hands.Add(splittedInput[0], int.Parse(splittedInput[1]));
-                //Console.WriteLine($"hand: {splittedInput[0]} - bid: {splittedInput[1]}");
-            }
-
-            return hands;
         }
     }
 
---
+++ AoC/hand.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC2023_Exo7
+{
+    internal class hand : Hand
+    {
+        public hand(string _cards, int _bid) : base(_cards, _bid)
+        {
+        }
+    }
+}
